2866feea08a6586f7f81cd445f3dcfda7a6dd5bd
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2025-05-13 20:32:36 -0300

    Incorpore Lombok a las clases de Libro, Usuario y Prestamo

--- gestion/src/main/java/com/biblioteca/gestion/Libro.java
+++ gestion/src/main/java/com/biblioteca/gestion/Libro.java
@@ -1,63 +1,16 @@
 package com.biblioteca.gestion;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Libro {
     private int id;
     private String isbn;
     private String titulo;
     private String autor;
     private EstadodeLibro estado;
-
-    // Constructor vacío
-    public Libro() {}
-
-    // Constructor completo
-    public Libro(int id, String isbn, String titulo, String autor, EstadodeLibro estado) {
-        this.id = id;
-        this.isbn = isbn;
-        this.titulo = titulo;
-        this.autor = autor;
-        this.estado = estado;
-    }
-
-    // Getters y setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public EstadodeLibro getEstado() {
-        return estado;
-    }
-
-    public void setEstado(EstadodeLibro estado) {
-        this.estado = estado;
-    }
 }
-
--- gestion/src/main/java/com/biblioteca/gestion/Prestamo.java
+++ gestion/src/main/java/com/biblioteca/gestion/Prestamo.java
@@ -1,65 +1,18 @@
 package com.biblioteca.gestion;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Prestamo {
     private int id;
     private String libro;
     private String usuario;
     private Date fechaPrestamo;
     private Date fechaDevolucion;
-
-    // Constructor vacío
-    public Prestamo() {}
-
-    // Constructor completo
-    public Prestamo(int id, String libro, String usuario, Date fechaPrestamo, Date fechaDevolucion) {
-        this.id = id;
-        this.libro = libro;
-        this.usuario = usuario;
-        this.fechaPrestamo = fechaPrestamo;
-        this.fechaDevolucion = fechaDevolucion;
-    }
-
-    // Getters y setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLibro() {
-        return libro;
-    }
-
-    public void setLibro(String libro) {
-        this.libro = libro;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public Date getFechaPrestamo() {
-        return fechaPrestamo;
-    }
-
-    public void setFechaPrestamo(Date fechaPrestamo) {
-        this.fechaPrestamo = fechaPrestamo;
-    }
-
-    public Date getFechaDevolucion() {
-        return fechaDevolucion;
-    }
-
-    public void setFechaDevolucion(Date fechaDevolucion) {
-        this.fechaDevolucion = fechaDevolucion;
-    }
 }
-
