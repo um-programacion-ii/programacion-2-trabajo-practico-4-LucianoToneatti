506394d010a66b70c1be0b436f8dc38261db9334
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2025-05-13 21:25:11 -0300

    Implementación de los Services

---
+++ gestion/src/main/java/com/biblioteca/gestion/services/PrestamoService.java
@@ -0,0 +1,14 @@
+package com.biblioteca.gestion.services;
+
+import com.biblioteca.gestion.models.Prestamo;
+
+import java.util.List;
+
+public interface PrestamoService {
+    Prestamo buscarPorId(Long id);
+    List<Prestamo> obtenerTodos();
+    Prestamo guardar(Prestamo prestamo);
+    void eliminar(Long id);
+    Prestamo actualizar(Long id, Prestamo prestamo);
+}
+
